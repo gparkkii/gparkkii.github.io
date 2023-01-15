9da18a83b91406eb3249b4bc2cc8d63a956775ed
gparkkii <jyp933@gmail.com>
2023-01-15 15:28:32 +0900

    chore: install typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gparkkii-Gatsby`,
+    description: `Developer gparkkii's blog.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {    
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+      isTSX: true,
+      allExtensions: true,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
