e5ba3319bf96805505f5a014ae1d11ed40681482
gparkkii <jyp933@gmail.com>
2023-04-29 06:12:12 +0900

    fix: remove unnecessary code

--- src/components/Post/Tag.tsx
+++ src/components/Post/Tag.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link, navigate } from 'gatsby';
 import { theme } from 'theme/index';
 import { Tag as TagTypo } from 'styles/typography';
 
 const TagBox = styled.button`
   display: inline-block;
-  margin-right: 12px;
+  margin-right: 16px;
   border-radius: 1000px;
-`;
-
-const LineAnimation = styled.div`
-  & p::after {
-    display: block;
-    content: '';
-    width: 0;
-    height: 2px;
-    margin-bottom: 2px;
-    margin-top: -4px;
-    background-color: ${props => props.theme.colors.primary[300]};
-    transition: width 0.3s;
-  }
-  &:hover {
-    & p::after {
-      width: 100%;
-    }
-  }
 `;
 
 interface TagProps {
@@ -34,10 +15,8 @@ interface TagProps {
 
 const Tag = ({ tag }: TagProps) => {
   return (
-    <TagBox onClick={() => navigate(`/?tag=${tag}`)}>
-      <LineAnimation>
-        <TagTypo textColor={theme.colors.primary.default}>#{tag}</TagTypo>
-      </LineAnimation>
+    <TagBox>
+      <TagTypo textColor={theme.colors.primary.default}>#{tag}</TagTypo>
     </TagBox>
   );
 };
